46d466971c1f7d244426b35559c4cacc0da4fa51
panktiB <pankti.bhadresa@vphrase.com>
2021-10-10 17:19:59 +0100

    update: basic logic to fetch data from given API and simply render along with basic search bar for searching in all fields of the list of users

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,11 +1,75 @@
 <template>
   <vs-row>
-    landing page
+    <vs-col
+      vs-w="12"
+    >
+      <search-bar
+        @change="handleSearch"
+      />
+      <div
+        v-for="user in filteredUsers"
+        :key="user['id']"
+        class="p-5"
+      >
+        {{ user['name'] }}
+      </div>
+    </vs-col>
   </vs-row>
 </template>
 
 <script>
+  import axios from 'axios';
+  import SearchBar from './SearchBar';
+
   export default {
     name: 'LandingPage',
+    components: { SearchBar },
+    data: function () {
+      return {
+        users: [],
+        searchKeyword: '',
+      };
+    },
+    computed: {
+      filteredUsers: function () {
+        if(this.searchKeyword.length) {
+          let searchWord = this.searchKeyword.toLowerCase();
+          let searchFields = ['name', 'email', 'role'];
+          let a = this.users.filter(user => {
+            return user['name'].toLowerCase().includes(searchWord) ||
+              user['email'].toLowerCase().includes(searchWord) ||
+              user['role'].toLowerCase().includes(searchWord);
+          });
+          return a;
+        } else {
+          return this.users;
+        }
+      }
+    },
+    mounted () {
+      this.getUsers();
+    },
+    methods: {
+      getUsers: function () {
+        this.fetchUsers().then(users => {
+          this.users = users;
+        }).catch(error => {
+          console.log(error);
+        });
+      },
+      fetchUsers: function () {
+        return new Promise((resolve, reject) => {
+          axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json').then(response => {
+            resolve(response['data']);
+          }).catch(error => {
+            reject(error);
+          });
+        });
+      },
+      handleSearch: function (searchKeyword) {
+        console.log(searchKeyword);
+        this.searchKeyword = searchKeyword;
+      }
+    }
   };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <vs-row class="width-100">
+    <vs-input
+      v-model="searchKeyword"
+      class="width-100"
+      @input="emitEvent"
+    />
+  </vs-row>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    data: function () {
+      return {
+        searchKeyword: '',
+      };
+    },
+    methods: {
+      emitEvent: function () {
+        this.$emit('change', this.searchKeyword);
+      }
+    }
+  };
+</script>
